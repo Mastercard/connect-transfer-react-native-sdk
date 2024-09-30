c0a76a18b3fcc62dcf26dad67cddfdb6dd5daf71
Sanjili Patkar <sanjili.patkar@mastercard.com>
2024-09-30 20:07:36 +0530

    CONM-376: spatkar:  React Native-PDS: ConnectTransfer [Initial Setup]- Localization Setup

--- index.tsx
+++ index.tsx
@@ -1,13 +1,17 @@
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 
 import store from './src/redux/store';
 import LandingScreen from './src/containers/LandingScreen';
+import i18next from './src/locale/i18n';
 
 const App: React.FC = () => (
-  <Provider store={store}>
-    <LandingScreen />
-  </Provider>
+  <I18nextProvider i18n={i18next}>
+    <Provider store={store}>
+      <LandingScreen />
+    </Provider>
+  </I18nextProvider>
 );
 
 AppRegistry.registerComponent('ConnectTransferReactNativeSdk', () => App);
--- src/containers/LandingScreen.tsx
+++ src/containers/LandingScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button, SafeAreaView, StyleSheet, Text } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 import { setUser, clearUser } from '../redux/slices/authSlice';
 import { RootState } from '../redux/store';
@@ -9,6 +10,7 @@ import { apiKeys } from '../services/api/apiKeys';
 
 function LandingScreen(): React.JSX.Element {
   const dispatch = useDispatch();
+  const { t, i18n } = useTranslation();
 
   const name = useSelector((state: RootState) => state.user.name);
 
@@ -24,6 +26,21 @@ function LandingScreen(): React.JSX.Element {
     dispatch(authenticateUser(apiKeys.authenticateUser));
   };
 
+  const testTranslation = () => {
+    const changeLanguage = language => {
+      i18n.changeLanguage(language);
+    };
+
+    return (
+      <View style={{ padding: 20 }}>
+        <Text style={{ fontSize: 20, paddingBottom: 50 }}>First {t('ErrorTitle')}</Text>
+        <Text style={{ fontSize: 20 }}>Second {t('ErrorSubtitle')}</Text>
+        <Button title="English" onPress={() => changeLanguage('en')} />
+        <Button title="Español" onPress={() => changeLanguage('es')} />
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView>
       <Text style={styles.sectionTitle}>Landing Screen</Text>
@@ -31,6 +48,7 @@ function LandingScreen(): React.JSX.Element {
       <Button title="Set User" onPress={handleSetUser} />
       <Button title="Clear User" onPress={handleClearUser} />
       <Button title="Authenticate API" onPress={handleAuthenticate} />
+      {testTranslation()}
     </SafeAreaView>
   );
 }
